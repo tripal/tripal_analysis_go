b7c32ec3d68f88717b1a3ef6b2b631b85fc0bb75
spficklin <spficklin@gmail.com>
2012-08-22 02:09:08 -0400

    Adjusted templates so they don't show when no data is present. Also added more details for administrator

--- theme/tripal_organism/tripal_organism_go_summary.tpl.php
+++ theme/tripal_organism/tripal_organism_go_summary.tpl.php
@@ -30,11 +30,18 @@ else {
         There are no GO reports available
         <p><br>Administrators, to view a GO report you must:
         <ul>
-          <li>load GO assignments and associate them to features and a corresponding analysis</li>
+          <li>Load GO assignments. GO assignments can be part of a GFF3 file and
+             loaded using the <a href="<?php print url('admin/tripal/tripal_feature/gff3_load');?>">GFF loader</a> 
+             or in a GAF format file and loaded using the <a href="<?php print url('admin/tripal/tripal_analysis_go/gaf_load');?>">GAF loader</a>.
+             Tools such as Blast2GO are capable of assigning GO terms to features and generating GAF files. Additionally, the Tripal InterProScan Extension module parses InterProScan XML 
+             output and imports GO terms associated with features. The InterProScan Extension Module is obtained separately 
+             from the core Tripal package.</li>
           <li>Set the <a href="<?php print url('admin/tripal/tripal_cv/cvtermpath');?>">cvtermpath</a> for the 'biological process', 'molecular_function' and 'cellular_component' vocabularies.</li>
           <li>Populate the <a href="<?php print url('admin/tripal/tripal_mviews');?>">go_count_analysis</a> materialized view</li>
+          <li>Ensure the user <a href="<?php print url('admin/user/permissions'); ?>"> has permission</a> to view the GO analysis content</li>          
         </ul> 
         </p>
+        This page will not appear to site visitors unless results are present.
       </div>
     </div>
     <?php
